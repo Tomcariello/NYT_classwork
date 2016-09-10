4d6f44bd3e9160532224ad1e2190fca7f5736449
Tomcariello <tomcariello@yahoo.com>
2016-09-10 13:49:59 -0400

    javascript mostly working

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,49 @@
 
 // Built by LucyBot. www.lucybot.com
 
-function searchResults() {
-  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-  url += '?' + $.param({
-    'q': "ufc"
-  });
+function searchResults(searchTerm) {
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  url += searchTerm;
+  // ?begin_date=YYYYMMDD
+  // ?end_date=YYYYMMDD
 
   $.ajax({url: url, method: 'GET',}).done(function(result) {
-      console.log(result);
+
+    for (i =1; i < 6; i++) {
+
+      var mainDiv = $('<div>');
+      mainDiv.addClass('panel panel-info')
+
+        var secondDiv = $('<div>');
+        secondDiv.addClass('panel-heading');
+        
+        var header = $('<h3>');
+        header.addClass('panel-title');
+        header.text("result: " + i);
+
+        secondDiv.append(header);
+
+      var summary = $('<div>');
+      summary.addClass('panel-body');
+
+      var p = $('<p>');
+      var a = $('<a>');
+      var p_date = $('<p>');
+
+      p_date.text(result.response.docs[i].pub_date);
+      a.attr('href', result.response.docs[i].web_url);
+      a.text(result.response.docs[i].snippet);
+      p.append(a);
+      p.append(p_date);
+
+      summary.append(p);
+
+      secondDiv.append(summary);
+      mainDiv.append(secondDiv);
+
+      $('#resultDiv').append(mainDiv);
+      console.log("Panel written: " + i);
+    }
     }).fail(function(err) {
       throw err;
   });
@@ -19,6 +54,12 @@ function searchResults() {
 
 // Process Form Submittal
 $('#submit').on('click', function() {
-  alert("clicked");
+  console.log(searchTerm);
+  var submittedString = $("#searchTerm").val();
+  // var submittedStartDate = $("#startDate").val();
+  // var submittedEndDate = $("#endDate").val();
+  // var recordsRequested = $("#records").val();
+  // alert("clicked " + submittedString);
+  searchResults("submittedString");
   return false;
 });
